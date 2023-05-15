e9d149487e9a85b0b4ff8dd048677599e278b864
Abouyacoub Oussama <oussamaabouyacoub11@gmail.com>
2023-05-15 21:36:20 +0100

    adding files

--- Water Billing App/Agents.cs
+++ Water Billing App/Agents.cs
@@ -19,25 +19,40 @@ namespace Water_Billing_App
         }
 
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\oussa\OneDrive\Documents\WaterBillingDB.mdf;Integrated Security=True;Connect Timeout=30");
-
+        private void showAgents()
+        {
+            con.Open();
+            string Query = "select * from AgentTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+            SqlCommandBuilder Builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            /*sda.Fill(ds);*/
+            AgentsDGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
         private void SaveBtn_Click(object sender, EventArgs e)
         {
-            if (AgNameTb.Text == "" || AgAddTb.Text == "" || AgPhoneTb.Text == "" || AgPassTb.Text == "") {
+            if (AgNameTb.Text == "" || AgAddTb.Text == "" || AgPhoneTb.Text == "" || AgPassTb.Text == "")
+            {
                 MessageBox.Show("Missing Informations!!!");
-            }else
+            }
+            else
             {
                 try
                 {
                     con.Open();
-                    SqlCommand cmd = new("insert into AgentTbl values('" + AgNameTb.Text + "','" + AgAddTb.Text + "','" + AgPhoneTb.Text + "','" + AgPassTb.Text + "')", con);
+                    SqlCommand cmd = new SqlCommand("insert into AgentTbl(AgName, AgPhone, AgAdd, AgPass) values(@AN, @AP, @AA, @APa)", con);
+                    cmd.Parameters.AddWithValue("@AN", AgNameTb.Text);
+                    cmd.Parameters.AddWithValue("@AP", AgPhoneTb.Text);
+                    cmd.Parameters.AddWithValue("@AA", AgAddTb.Text);
+                    cmd.Parameters.AddWithValue("@APa", AgPassTb.Text);
                     cmd.ExecuteNonQuery();
-                    MessageBox.Show("Agent Saved Successfully");
+                    MessageBox.Show("Agent Saved Successfully!");
                     con.Close();
                     AgNameTb.Text = "";
                     AgAddTb.Text = "";
                     AgPhoneTb.Text = "";
                     AgPassTb.Text = "";
-                    con.Close();
                 }
                 catch (Exception Ex)
                 {
